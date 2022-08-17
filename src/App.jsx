db336950a4506f86e24f9d0aeb5f4d3e1c078d3d
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-08-17 02:39:54 -0300

    desktop app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,136 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import questions from "./questions";
+import "./global.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [preguntaActual, setPreguntaActual] = useState(0);
+  const [puntuacion, setPuntuacion] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [tiempoRestante, setTiempoRestante] = useState(10);
+  const [areDisabled, setAreDisabled] = useState(false);
+  const [answerShow, setAnswerShow] = useState(false);
+
+  function handleAnswerSubmit(isCorrect) {
+    if (isCorrect) setPuntuacion(puntuacion + 1);
+    if (preguntaActual === questions.length - 1) {
+      setIsFinished(true);
+    } else {
+      setPreguntaActual(preguntaActual + 1);
+    }
+    setTiempoRestante(10);
+  }
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      if (tiempoRestante > 0) setTiempoRestante((prev) => prev - 1);
+      if (tiempoRestante === 0) setAreDisabled(true);
+    }, 1000);
+
+    return () => clearInterval(intervalo);
+  }, [tiempoRestante]);
+
+  if (isFinished) {
+    return (
+      <main className="App container-juego-terminado">
+        <div className="juego-terminado">
+          <span>{`You got ${puntuacion} out of ${questions.length}`}</span>
+          <div className="buttons-juego-terminado">
+            <button
+              onClick={() => {
+                window.location.href = "/";
+              }}
+            >
+              Play again
+            </button>
+            <button
+              onClick={() => {
+                setIsFinished(false);
+                setAnswerShow(true);
+                setPreguntaActual(0);
+              }}
+            >
+              View answers
+            </button>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  if (answerShow) {
+    return (
+      <main className="App">
+        <div className="lado-izquierdo agregado-izquierdo">
+          <div className="numero-pregunta">
+            <span>Question {preguntaActual + 1} of</span> {questions.length}
+          </div>
+          <div className="titulo-pregunta">
+            {questions[preguntaActual].titulo}
+          </div>
+          <div className="response">
+            {
+              questions[preguntaActual].options.filter(
+                (option) => option.isCorrect
+              )[0].textResponse
+            }
+          </div>
+          <button
+            className="btn-final-juego"
+            onClick={() => {
+              if (preguntaActual === questions.length - 1) {
+                window.location.href = "/";
+              } else {
+                setPreguntaActual(preguntaActual + 1);
+              }
+            }}
+          >
+            {preguntaActual === questions.length - 1 ? "Play again" : "Next"}
+          </button>
+        </div>
+      </main>
+    );
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="App">
+      <div className="ambos-lados">
+        <div className="lado-izquierdo">
+          <div className="numero-pregunta">
+            Question {preguntaActual + 1} of {questions.length}
+          </div>
+          <div className="titulo-pregunta">
+            {questions[preguntaActual].titulo}
+          </div>
+          <div className="tiempo-restante">
+            {!areDisabled ? (
+              <span>{`Time remaining ${tiempoRestante}`}</span>
+            ) : (
+              <button
+                onClick={() => {
+                  setTiempoRestante(10);
+                  setAreDisabled(false);
+                  setPreguntaActual(preguntaActual + 1);
+                }}
+              >
+                Continue
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="lado-derecho">
+          {questions[preguntaActual].options.map((response, index) => (
+            <button
+              disabled={areDisabled}
+              key={index}
+              onClick={(e) => handleAnswerSubmit(response.isCorrect)}
+            >
+              {response.textResponse}
+            </button>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/questions.js
@@ -0,0 +1,95 @@
+const questions = [
+  {
+    titulo: "In what year did Margaret Thatcher die ?",
+    options: [
+      { textResponse: "2013", isCorrect: true },
+      { textResponse: "2016", isCorrect: false },
+      { textResponse: "2007", isCorrect: false },
+      { textResponse: "2003", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "In what year was the first trip to the Moon launched?",
+    options: [
+      { textResponse: "1958", isCorrect: false },
+      { textResponse: "1962", isCorrect: false },
+      { textResponse: "1967", isCorrect: false },
+      { textResponse: "1969", isCorrect: true },
+    ],
+  },
+  {
+    titulo: "What is the largest country in the world?",
+    options: [
+      { textResponse: "China", isCorrect: false },
+      { textResponse: "India", isCorrect: false },
+      { textResponse: "Rusia", isCorrect: true },
+      { textResponse: "Brasil", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "How many ribs does the average human have?",
+    options: [
+      { textResponse: "18", isCorrect: false },
+      { textResponse: "20", isCorrect: false },
+      { textResponse: "22", isCorrect: false },
+      { textResponse: "24", isCorrect: true },
+    ],
+  },
+  {
+    titulo: "In what year was the movie Titanic released ?",
+    options: [
+      { textResponse: "1995", isCorrect: false },
+      { textResponse: "1997", isCorrect: true },
+      { textResponse: "2001", isCorrect: false },
+      { textResponse: "2003", isCorrect: false },
+    ],
+  },
+  {
+    titulo:
+      "What is the approximate size of the small intestine of an adult?",
+    options: [
+      { textResponse: "Between 4 and 5 meters in length", isCorrect: false },
+      { textResponse: "Between 5 and 6 meters in length", isCorrect: false },
+      { textResponse: "Between 6 and 7 meters in length", isCorrect: true },
+      { textResponse: "Between 7 and 8 meters in length", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "From which country does the mojito cocktail originate?",
+    options: [
+      { textResponse: "Cuba", isCorrect: true },
+      { textResponse: "Panama", isCorrect: false },
+      { textResponse: "Puerto Rico", isCorrect: false },
+      { textResponse: "Jamaica", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "Where is Mount Fuji located?",
+    options: [
+      { textResponse: "Corea del Sur", isCorrect: false },
+      { textResponse: "Japón", isCorrect: true },
+      { textResponse: "India", isCorrect: false },
+      { textResponse: "Nueva Zelanda", isCorrect: false },
+    ],
+  },
+  {
+    titulo: "In what year did Freddy Mercury die ?",
+    options: [
+      { textResponse: "1985", isCorrect: false },
+      { textResponse: "1987", isCorrect: false },
+      { textResponse: "1989", isCorrect: false },
+      { textResponse: "1991", isCorrect: true },
+    ],
+  },
+  {
+    titulo: "What is the highest mountain on the planet?",
+    options: [
+      { textResponse: "Everest", isCorrect: true },
+      { textResponse: "K2", isCorrect: false },
+      { textResponse: "Tirich Mir", isCorrect: false },
+      { textResponse: "Aconcagua", isCorrect: false },
+    ],
+  }
+];
+
+export default questions;
